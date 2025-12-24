f4650b5cfb2b21c84dd1dec3517212ebc1fdf410
Adam Nouira <adam.nouira@etu.unistra.fr>
2025-12-24 01:45:51 +0100

    m

--- config/firebase.ts
+++ config/firebase.ts
@@ -3,14 +3,13 @@ import { Platform } from 'react-native';
 
 // Firebase configuration from your .env
 const firebaseConfig = {
-  apiKey: "AIzaSyDc2y0sq9ngz6zvXJxf1XEuQmb9YcbW4s8",
-  authDomain: "chatty-adde4.firebaseapp.com",
-  projectId: "chatty-adde4",
-  storageBucket: "chatty-adde4.firebasestorage.app",
-  messagingSenderId: "770730888216",
-  appId: "1:770730888216:web:89a4920c081defeef4476d"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
-
 let auth: any;
 let firestore: any;
 
@@ -19,19 +18,19 @@ if (Platform.OS === 'web') {
   const { initializeApp } = require('firebase/app');
   const { getAuth } = require('firebase/auth');
   const { getFirestore } = require('firebase/firestore');
-  
+
   const app = initializeApp(firebaseConfig);
   auth = () => getAuth(app);
   firestore = () => getFirestore(app);
-  
+
 } else {
   // Use React Native Firebase for iOS/Android
   const rnAuth = require('@react-native-firebase/auth').default;
   const rnFirestore = require('@react-native-firebase/firestore').default;
-  
+
   auth = rnAuth;
   firestore = rnFirestore;
-  
+
   // Enable Firestore offline persistence for mobile
   firestore().settings({
     persistence: true,
